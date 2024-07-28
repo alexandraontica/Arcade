e7761ec4de75cc2b353b2e9a33216920a56817be
Alexandra Onțică <alexandra.ontica@gmail.com>
2024-07-28 15:25:18 +0300

    refactored main.py

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from turtle import Screen
 from button import Button
 
-SNAKE = 0
-PONG = 1
-CROSS = 2
-RACE = 3
+GAMES = [
+    {"game_num": 0, "name": "Snake", "color": "red", "position": (-110, 50)},
+    {"game_num": 1, "name": "Pong", "color": "blue", "position": (110, 50)},
+    {"game_num": 2, "name": "Turtle Crossing", "color": "yellow", "position": (-110, -70)},
+    {"game_num": 3, "name": "Turtles Race", "color": "green", "position": (110, -70)},
+]
 
 screen = Screen()
 screen.setup(width=810, height=900)
@@ -12,18 +14,11 @@ screen.title("PyArcade")
 screen.bgpic("assets/arcade-game.gif")
 screen.tracer(0)
 
-snake_game_button = Button(game_num=SNAKE, game_name="Snake", color="red", position=(-110, 50), screen=screen)
-snake_game_button.onclick(snake_game_button.on_click)
-
-pong_button = Button(game_num=PONG, game_name="Pong", color="blue", position=(110, 50), screen=screen)
-pong_button.onclick(pong_button.on_click)
-
-cross_button = Button(game_num=CROSS, game_name="Turtle Crossing", color="yellow", position=(-110, -70), screen=screen)
-cross_button.onclick(cross_button.on_click)
-
-race_button = Button(game_num=RACE, game_name="Turtles Race", color="green", position=(110, -70), screen=screen)
-race_button.onclick(race_button.on_click)
+buttons = []
+for game in GAMES:
+    button = Button(game_num=game["game_num"], game_name=game["name"], color=game["color"], position=game["position"], screen=screen)
+    button.onclick(button.on_click)
+    buttons.append(button)
 
 screen.update()
-
 screen.exitonclick()
